1bdf6086466e6e106f48697b9df628cb086c7ca4
Eugene <eugenejacksonjoy@gmail.com>
2025-09-17 09:41:27 +0800

    update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
@@ -10,13 +10,13 @@ export default function Home() {
                 className="text-center text-white d-flex flex-column justify-content-center align-items-center"
                 style={{
                     height: "100vh",
-                    backgroundImage: "url('/ public / images / backgorund.jpg')",
+                    backgroundImage: 'url("/images/background.jpg")',
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                 }}
             >
-                <h1 className="display-3 mb-4">Cooking Todo App</h1>
-                <p className="lead mb-4">Plan your day and save your favourite recipes!</p>
+                <h2 style={{ color: "black" }} className=" text-black bg-white display-3 mb-4">Welcome to Cooking App</h2>
+                <p style={{ color: "black" }} className="text-black bg-white lead mb-4">Plan your day and save your favourite recipes!</p>
                 <Button
                     variant="primary"
                     size="lg"
@@ -25,20 +25,6 @@ export default function Home() {
                     Get Started
                 </Button>
             </div>
-
-            <Container className="my-5">
-                <Row xs={1} md={3} className="g-4">
-                    <Col>
-                        <Image src="./public/images/image1.jpg" rounded fluid />
-                    </Col>
-                    <Col>
-                        <Image src="./public/images/image2.jpg" rounded fluid />
-                    </Col>
-                    <Col>
-                        <Image src="./public/images/image3.jpg" rounded fluid />
-                    </Col>
-                </Row>
-            </Container>
 
         </Container>
     );
